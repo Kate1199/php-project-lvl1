3889d198c5edab2a022f05985650559d9e31b11f
Kate1199 <kate.kisel99@yandex.by>
2022-01-20 13:07:52 +0300

    add player greeting

---
+++ src/Cli.php
@@ -0,0 +1,16 @@
+<?php
+namespace Brain\Games\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function greetPlayer()
+{
+    $welcomeMessage = "Welcome to the Brain Games!";
+    $askForName = "May I have your name?";
+    $greet = "Hello, %s";
+
+    line($welcomeMessage);
+    $name = prompt($askForName);
+    line($greet, $name);
+}
